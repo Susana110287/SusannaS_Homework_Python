aafd7d2313d3833f49f2c7cfbc9fc81dbcd4632e
Susana110287 <susana11.02.87@gmail.com>
2026-01-24 16:15:28 +0700

    lesson8 # Changes

--- LESSON_8/LESSON_8/test_create_company.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-base_url = "https://ru.yougile.com"
-
-
-def test_add_new():
-    resp = requests.get(base_url+'/company/list')  # Получаем список
-    body = resp.json()  # Сохраняем его в переменную
-    len_before = len(body)  # Находим длину переменной
