eb9fc84f717c94716590939f0fad635f3ea878ee
Susana110287 <susana11.02.87@gmail.com>
2026-01-26 23:17:13 +0700

    Changes

---
+++ LESSON_8/project_YouGile.py
@@ -0,0 +1,69 @@
+import requests
+
+
+class ProjectYouGile:
+    # Инициализация
+    def __init__(self, url) -> None:
+        self.url = url
+
+    # Получить ключ авторизации
+    def get_token(self):
+        payload = {
+            "login": "susana11.02.87@gmail.com",
+            "password": "Lili30*09*09",
+            "companyId": "4e6100c8-c730-4121-b312-d60504f45f23"
+        }
+        resp = requests.post(self.url + 'auth/keys/get', json=payload)
+        return resp.json()[0]['key']
+
+    # Получить список проектов
+    def get_project_list(self):
+        key = self.get_token()
+        headers = {
+            'Authorization': f'Bearer {key}',
+            'Content-Type': 'application/json'
+        }
+
+        resp = requests.get(self.url + 'projects', headers=headers)
+        return resp.json()['content']
+
+    # Добавить проект:
+    def create_project(self, title, users):
+        key = self.get_token()
+        headers = {
+            'Authorization': f'Bearer {key}',
+            'Content-Type': 'application/json'
+        }
+        body = {
+            "title": title,
+            "users": users
+        }
+        resp = requests.post(self.url + 'projects',
+                             headers=headers,
+                             json=body)
+        return resp.json()
+
+    # Получить проект по id
+    def get_project_with_id(self, project_id):
+        key = self.get_token()
+        headers = {
+            'Authorization': f'Bearer {key}'
+        }
+        resp = requests.get(self.url + f'projects/{project_id}',
+                            headers=headers)
+        return resp.json()
+
+    # Изменить название проекта
+    def edit_project(self, project_id, new_deleted, new_title):
+        key = self.get_token()
+        headers = {
+            'Authorization': f'Bearer {key}',
+            'Content-Type': 'application/json'
+        }
+        project = {
+            "deleted": new_deleted,
+            "title": new_title
+        }
+        resp = requests.put(self.url + f'projects/{project_id}',
+                            headers=headers, json=project)
+        return resp.json()
---
+++ LESSON_8/test_project_YouGile_negative.py
@@ -0,0 +1,51 @@
+from project_YouGile import ProjectYouGile
+
+
+name = "Моя компания"
+users = {"0abc98f7-5c87-4694-a33d-567c2a60df6b": "admin"}
+
+
+api = ProjectYouGile('https://ru.yougile.com/api-v2/')
+
+
+def test_create_project():
+    # количество проектов до
+    projects_before = api.get_project_list()
+    len_before = len(projects_before)
+
+    # создание проекта
+    project_id = api.create_project('', users)
+
+    # количество проектов после
+    projects_after = api.get_project_list()
+    len_after = len(projects_after)
+
+    assert len_after - len_before == 1
+    # удаляем созданный проект
+    api.edit_project(project_id, True, 'Del_Project_Test')
+
+
+def test_get_project_with_id():
+    title = 'Тест получение ID проекта'
+    result = api.create_project(title, users)
+    project_id = result['']
+
+    # обращаемся к проекту
+    new_project = api.get_project_with_id(project_id)
+
+    assert new_project['title'] == title
+    assert new_project['users'] == users
+
+
+def test_edit_project():
+    title = 'Тест изменение проекта'
+
+    result = api.create_project(title, users)
+
+    project_id = result['id']
+
+    api.edit_project(project_id, False, 'Новое название №1')
+
+    edited = api.get_project_with_id(project_id)
+
+    assert edited['title'] == 'Новое название №2'
